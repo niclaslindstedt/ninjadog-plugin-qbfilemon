14f8e350290cdd60b7ed15ac6230843a12d73633
Joachim Lindström <lindstrm@gmail.com>
2018-07-21 20:01:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const Base = require('ninjakatt-plugin-base');
+const chokidar = require('chokidar');
+const axios = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+const { filename } = require(`${global.appRoot}/lib/helpers`);
+
+const config = {
+  responseType: 'arraybuffer'
+};
+const emitter = global.emitter;
+
+module.exports = class Files extends Base {
+  constructor() {
+    super(__dirname);
+    this.setupListeners();
+    this.http = null;
+  }
+
+  setup() {
+    const watcher = chokidar.watch(this.settings.watchDir);
+    watcher
+      .on('add', path => {
+        global.emitter.emit('file.add', path);
+      })
+      .on('change', path => {
+        global.emitter.emit('file.change', path);
+      })
+      .on('error', error => {});
+  }
+
+  async downloadFile(url, savePath, httpconfig, callback) {
+    httpconfig = { ...config, ...httpconfig };
+    const file = await axios
+      .get(url, httpconfig)
+      .then(response => response.data);
+    savePath = path.resolve(savePath, filename(url));
+    await fs.writeFile(savePath, file);
+    global.emitter.emit('message', `Downloaded ${savePath}`, 'add');
+
+    if (callback) {
+      return callback(savePath);
+    }
+
+    return Promise.resolve(savePath);
+  }
+
+  setupListeners() {
+    emitter.register(
+      'file.save',
+      async (path, data) => {
+        await fs.ensureFile(path);
+        await fs.writeFile(path, data);
+        return Promise.resolve(path);
+      },
+      Files.name
+    );
+
+    emitter.register(
+      'file.download',
+      async (url, savePath, httpconfig, callback) => {
+        callback = typeof httpconfig === 'function' ? httpconfig : callback;
+        httpconfig = typeof httpconfig === 'function' ? null : httpconfig;
+        savePath = await this.downloadFile(url, savePath, httpconfig).catch(
+          e => {
+            emitter.emit(
+              'message',
+              `Error downloading ${url.split('/').pop()}, status: ${
+                e.response.status
+              }`,
+              'error'
+            );
+          }
+        );
+        if (callback) {
+          return callback(path);
+        } else {
+          return Promise.resolve(path);
+        }
+      },
+      Files.name
+    );
+  }
+};
