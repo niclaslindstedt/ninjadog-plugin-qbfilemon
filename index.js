f1ed07c4ef9c823d151dc353dc32d479d0d92fe7
Niclas Lindstedt <nicl@slindstedt.se>
2020-05-09 11:28:38 +0200

    Add worflow, editorconfig & use base functions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { filename } = require(`${global.appRoot}/lib/helpers`);
 
 const config = {
-  responseType: 'arraybuffer'
+  responseType: 'arraybuffer',
 };
 const emitter = global.emitter;
 
@@ -15,73 +15,67 @@ module.exports = class Files {
   }
 
   setup() {
-    this.setupListeners();
+    this.logDebug('Setting up files plugin');
+    this.setupFileWatcher();
+    this.subscribe('file.save', actOnFileSave);
+    this.subscribe('file.download', actOnFileDownload);
+  }
+
+  setupFileWatcher() {
     const watcher = chokidar.watch(this.settings.watchDir);
     watcher
-      .on('add', path => {
-        global.emitter.emit('file.add', path);
+      .on('add', (path) => {
+        this.logDiag(`File added: ${path}`);
+        this.emit('file.add', path);
       })
-      .on('change', path => {
-        global.emitter.emit('file.change', path);
+      .on('change', (path) => {
+        this.logDiag(`File changed: ${path}`);
+        this.emit('file.change', path);
       })
-      .on('error', error => {});
+      .on('error', (error) => {
+        this.logError(`Error from chokidar watch: ${error}`);
+      });
+  }
+
+  async actOnFileSave(path, data) {
+    this.logDiag(`Acting on saved file: ${path}`);
+    await fs.ensureFile(path);
+    await fs.writeFile(path, data);
+    return Promise.resolve(path);
+  }
+
+  async actOnFileDownload(url, savePath, httpconfig, callback) {
+    callback = typeof httpconfig === 'function' ? httpconfig : callback;
+    httpconfig = typeof httpconfig === 'function' ? null : httpconfig;
+    savePath = await this.downloadFile(url, savePath, httpconfig).catch((e) => {
+      this.logError(
+        `Error downloading ${savePath.split(path.sep).pop()}, status: ${e}`
+      );
+    });
+    if (callback) {
+      return callback(path);
+    } else {
+      return Promise.resolve(path);
+    }
   }
 
   async downloadFile(url, savePath, httpconfig, callback) {
     httpconfig = { ...config, ...httpconfig };
     const file = await axios
       .get(url, httpconfig)
-      .then(response => response.data);
+      .then((response) => response.data);
 
     if (!savePath.match('.')) {
       savePath = path.resolve(savePath, filename(url));
     }
 
     await fs.writeFile(savePath, file);
-    global.emitter.emit('message', `Downloaded ${savePath}`, 'add', Files.name);
+    this.logInfo(`Downloaded ${savePath}`);
 
     if (callback) {
       return callback(savePath);
     }
 
     return Promise.resolve(savePath);
-  }
-
-  setupListeners() {
-    emitter.register(
-      'file.save',
-      async (path, data) => {
-        await fs.ensureFile(path);
-        await fs.writeFile(path, data);
-        return Promise.resolve(path);
-      },
-      Files.name
-    );
-
-    emitter.register(
-      'file.download',
-      async (url, savePath, httpconfig, callback) => {
-        callback = typeof httpconfig === 'function' ? httpconfig : callback;
-        httpconfig = typeof httpconfig === 'function' ? null : httpconfig;
-        savePath = await this.downloadFile(url, savePath, httpconfig).catch(
-          e => {
-            emitter.emit(
-              'message',
-              `Error downloading ${savePath
-                .split(path.sep)
-                .pop()}, status: ${e}`,
-              'error',
-              Files.name
-            );
-          }
-        );
-        if (callback) {
-          return callback(path);
-        } else {
-          return Promise.resolve(path);
-        }
-      },
-      Files.name
-    );
   }
 };
